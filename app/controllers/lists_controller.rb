20f3fe2f0b8bf641b3b4a3e33e8a5c0bcef9e3c0
Koty Wong <wongkoty@gmail.com>
2017-03-13 12:48:50 -0400

    show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,30 @@
 class ListsController < ApplicationController
   def index
-    render json: 'index of lists'
+    # render json: 'index of lists'
     lists = List.all;
+    render json: lists
   end
+
+  def show 
+    render json: List.find(params[:id])
+  end
+
+  def create 
+    puts 'create'
+    list = List.new(list_params)
+    puts params
+
+    if list.save
+      render json: 'saved'
+    else
+      render json: 'error'
+    end
+  end
+
+  private 
+
+    def list_params
+      params.require(:list).permit(:title)
+    end
+
 end
