9601a1ebc3441999f0e8be2df53cfc795bb5e087
Koty Wong <wongkoty@gmail.com>
2017-03-13 13:01:42 -0400

    update and delete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,9 +18,24 @@ class ListsController < ApplicationController
       puts 'saved'
       render json: @list, status: :created
     else
-      render json: !list.errors, status: :unprocessable_entity
+      render json: @list.errors, status: :unprocessable_entity
     end
   end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      render json: @list, status: :updated
+    else
+      render json: @list.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    List.find(params[:id]).destroy
+    render json: {status: 204}
+  end
+
 
   private 
 
